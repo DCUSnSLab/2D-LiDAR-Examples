334e206b89c950dc123a3f13d9e48dc9ff806883
junhp1234 <junhp12345@gmail.com>
2022-08-01 18:09:32 +0900

    Simple 2D LiDAR examples.

---
+++ scripts/Object_avoid.py
@@ -0,0 +1,43 @@
+import rospy
+from std_msgs.msg import Float64
+from sensor_msgs.msg import LaserScan
+
+class ObjectAvoid:
+    def __init__(self):
+        self.sub = rospy.Subscriber("/lidar2D", LaserScan, self.callback)
+
+        self.speed = rospy.Publisher('/commands/motor/speed', Float64, queue_size=1)
+        self.position = rospy.Publisher('/commands/servo/position', Float64, queue_size=1)
+
+    def callback(self, data):
+        """
+        입력되는 Lidar 데이터의 경우 0도를 기준으로 전방에서 시계방향으로 360도 각 방향을 가리킵니다.
+        따라서 ranges의 0 ~ 30, 330 ~ 359 영역을 전방으로 정의하여 간단한 장애물 회피 코드를 작성했습니다.
+        """
+        if min(data.ranges[0:30]) < 5 or min(data.ranges[330:359]) < 5:
+            """
+            전방 0 ~ 30도 또는 330 ~ 359도에 장애물이 있는 경우,
+            """
+            if min(data.ranges[0:30]) < min(data.ranges[330:359]):
+                """
+                왼쪽에 장애물이 있는 경우 우회전
+                """
+                self.speed.publish(7000)
+                self.position.publish(0.5304 + 0.2)
+            else:
+                """
+                오른쪽에 장애물이 있는 경우 좌회전한다.
+                """
+                self.speed.publish(7000)
+                self.position.publish(0.5304 - 0.2)
+        else:
+            self.speed.publish(10000)
+            self.position.publish(0.5304)
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node("ObjectAvoid")
+        run = ObjectAvoid()
+        rospy.spin()
+    except:
+        print("Error occured.")
---
+++ scripts/Wall_follwer.py
@@ -0,0 +1,113 @@
+import rospy
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf import transformations
+
+import math
+
+pub_ = None
+
+
+
+class WallFollower:
+    def __init__(self):
+        print("__init__ called.")
+        self.sub = rospy.Subscriber("/lidar2D", LaserScan, self.callback)
+
+        self.speed = rospy.Publisher('/commands/motor/speed', Float64, queue_size=1)
+        self.position = rospy.Publisher('/commands/servo/position', Float64, queue_size=1)
+
+        self.state_ = 0
+
+        self.state_dict_ = {
+            0: 'find the wall',
+            1: 'turn left',
+            2: 'follow the wall',
+        }
+
+        self.regions = None
+
+    def callback(self, data):
+        self.regions = {
+            'right': min(data.ranges[60:89]),
+            'fright': min(data.ranges[30:59]),
+            'front': min(min(data.ranges[0:29]), min(data.ranges[330:359])),
+            'fleft': min(data.ranges[300:329]),
+            'left': min(data.ranges[270:299]),
+        }
+
+        distance = 1.0
+
+        self.take_action(distance)
+
+        if self.state_ == 0:
+            self.find_wall()
+        elif self.state_ == 1:
+            self.turn_left()
+        elif self.state_ == 2:
+            self.follow_the_wall()
+            pass
+        else:
+            rospy.logerr('Unknown state!')
+
+    def change_state(self, state):
+        if state is not self.state_:
+            print('Wall follower - [%s] - %s' % (state, self.state_dict_[state]))
+            self.state_ = state
+
+    def take_action(self, distance):
+        d = distance
+
+        if self.regions['front'] > d and self.regions['fleft'] > d and self.regions['fright'] > d:
+            # state_description = 'case 1 - nothing'
+            self.change_state(0)
+        elif self.regions['front'] < d and self.regions['fleft'] > d and self.regions['fright'] > d:
+            # state_description = 'case 2 - front'
+            self.change_state(1)
+        elif self.regions['front'] > d and self.regions['fleft'] > d and self.regions['fright'] < d:
+            # state_description = 'case 3 - fright'
+            self.change_state(2)
+        elif self.regions['front'] > d and self.regions['fleft'] < d and self.regions['fright'] > d:
+            # state_description = 'case 4 - fleft'
+            self.change_state(0)
+        elif self.regions['front'] < d and self.regions['fleft'] > d and self.regions['fright'] < d:
+            # state_description = 'case 5 - front and fright'
+            self.change_state(1)
+        elif self.regions['front'] < d and self.regions['fleft'] < d and self.regions['fright'] > d:
+            # state_description = 'case 6 - front and fleft'
+            self.change_state(1)
+        elif self.regions['front'] < d and self.regions['fleft'] < d and self.regions['fright'] < d:
+            # state_description = 'case 7 - front and fleft and fright'
+            self.change_state(1)
+        elif self.regions['front'] > d and self.regions['fleft'] < d and self.regions['fright'] < d:
+            # state_description = 'case 8 - fleft and fright'
+            self.change_state(0)
+        else:
+            state_description = 'unknown case'
+            rospy.loginfo(self.regions)
+
+    def find_wall(self):
+        speed = 7000
+        position = 0.5304 - 0.4
+
+        self.speed.publish(speed)
+        self.position.publish(position)
+
+    def turn_left(self):
+        position = 0.5304 + 0.4
+
+        self.position.publish(position)
+
+    def follow_the_wall(self):
+        speed = 7000
+        position = 0.5304
+
+        self.speed.publish(speed)
+        self.position.publish(position)
+
+if __name__ == '__main__':
+    rospy.init_node("WallFollwer")
+    run = WallFollower()
+    rospy.spin()
