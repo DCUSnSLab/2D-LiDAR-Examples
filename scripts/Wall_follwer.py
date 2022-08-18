c5a435848d784065ae48241dcc25e1479ac8a2ea
soobinJeon <marsberry@cu.ac.kr>
2022-08-18 09:50:46 +0900

    updated wall following

--- scripts/Wall_follwer.py
+++ scripts/Wall_follwer.py
@@ -2,11 +2,6 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-from tf import transformations
-
-import math
 
 speed_pub = rospy.Publisher('/vesc/commands/motor/speed', Float64, queue_size=1)
 position_pub = rospy.Publisher('/vesc/commands/servo/position', Float64, queue_size=1)
@@ -14,12 +9,17 @@ position_pub = rospy.Publisher('/vesc/commands/servo/position', Float64, queue_s
 state_ = 0
 
 state_dict_ = {
-    0: 'find the wall',
-    1: 'turn left',
-    2: 'follow the wall',
+    0: 'away from the wall',
+    1: 'going to wall',
+    2: 'following the wall',
 }
 
 regions = None
+
+def degTorad(deg):
+	rad_diff = 0.5304
+	rad = deg * (3.14/180)
+	return rad + rad_diff
 
 def callback(data):
     global state_, regions
@@ -30,18 +30,26 @@ def callback(data):
         'fleft': min(data.ranges[300:329]),
         'left': min(data.ranges[270:299]),
     }
-
+    
+    #벽과의 최소 거리 설정
     distance = 2.0
-
+    
+    #차량의 상태 최신화
+    #상태 값, 0: away from the wall , 1: going to wall, 2:following the wall
     take_action(distance)
 
     print("state_", state_)
-
-    if state_ == 0:
-        find_wall()
-    elif state_ == 1:
+    
+    #차량은 왼쪽에 존재하는 벽을 추종함
+    #차량의 현재 상태에 따라 행동 결정
+    #차량이 벽에서 멀어지고 있을 때, 왼쪽으로 주행
+    #차량이 벽으로 다가가고 있을 때, 오른쪽으로 주행
+    #차량이 벽을 잘 따라가고 있을 때, 직진 주행
+    if state_ == 0: #away from the wall
         turn_left()
-    elif state_ == 2:
+    elif state_ == 1: #going to wall
+        turn_right()
+    elif state_ == 2: #following the wall
         follow_the_wall()
         pass
     else:
@@ -57,9 +65,18 @@ def take_action(distance):
     global regions
     d = distance
 
+    """
+    전방 영역의 값, 좌전방 영역의 값, 우전방 영역의 값이 모두 distance보다 큰 경우, 
+    벽에서 멀어지고 있는 상태이므로 상태를 0(away from the wall)으로 변경
+    """
     if regions['front'] > d and regions['fleft'] > d and regions['fright'] > d:
         # state_description = 'case 1 - nothing'
         change_state(0)
+    
+    """
+    전방 영역의 값이 distance보다 작고, 좌전방 영역의 값이 distance보다 크고, 우전방 영역의 값이 distance보다 큰 경우,
+    전방에 물체또는 벽이 있다고 판단되므로 going to wall로 변경
+    """
     elif regions['front'] < d and regions['fleft'] > d and regions['fright'] > d:
         # state_description = 'case 2 - front'
         change_state(1)
@@ -85,21 +102,21 @@ def take_action(distance):
         state_description = 'unknown case'
         rospy.loginfo(regions)
 
-def find_wall():
+def turn_left():
     speed = 7000
-    position = 0.5304 - 0.4
+    position = degTorad(-22)
 
     speed_pub.publish(speed)
     position_pub.publish(position)
 
-def turn_left():
-    position = 0.5304 + 0.4
+def turn_right():
+    position = degTorad(22)
 
     position_pub.publish(position)
 
 def follow_the_wall():
     speed = 7000
-    position = 0.5304
+    position = degTorad(22)
 
     speed_pub.publish(speed)
     position_pub.publish(position)
--- scripts/Wall_follwer_template.py
+++ scripts/Wall_follwer_template.py
@@ -1,140 +1,27 @@
 # -*- coding: utf-8 -*-
 import rospy
-from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-from tf import transformations
-
-import math
-
-speed_pub = rospy.Publisher('(시뮬레이터의 speed Subscriber와 동일하게 지정)', Float64, queue_size=1)
-position_pub = rospy.Publisher('(시뮬레이터의 position Subscriber와 동일하게 지정)', Float64, queue_size=1)
-
-state_ = 0
-
-state_dict_ = {
-    0: 'find the wall',
-    1: 'turn left',
-    2: 'follow the wall',
-}
-
-regions = None
+from sensor_msgs.msg import LaserScan
 
 def callback(data):
-    global state_, regions
-    regions = {
-        'right': min(data.ranges[60:89]),
-        'fright': min(data.ranges[30:59]),
-        'front': min(min(data.ranges[0:29]), min(data.ranges[330:359])),
-        'fleft': min(data.ranges[300:329]),
-        'left': min(data.ranges[270:299]),
-    }
-
-    distance = 2.0
-
-    take_action(distance)
-
     '''
-    각 상태에 따라 수행할 동작을 아래 조건문을 통해 판단한다.
+    Publisher speed,
     '''
-    if state_ == 0:
-        find_wall()
-    elif state_ == 1:
-        turn_left()
-    elif state_ == 2:
-        follow_the_wall()
-        pass
-    else:
-        rospy.logerr('Unknown state!')
-
-def change_state(state):
-    global state_
-    if state is not state_:
-        print('Wall follower - [%s] - %s' % (state, state_dict_[state]))
-        state_ = state
-
-def take_action(distance):
-    global regions
-    d = distance
-
-    if regions['front'] > d and regions['fleft'] > d and regions['fright'] > d:
-        """
-        전방 영역의 값, 좌전방 영역의 값, 우전방 영역의 값이 모두 distance보다 큰 경우,
-        차량이 오른쪽으로 이동하도록 상태를 갱신한다.
-        """
-    elif regions['front'] < d and regions['fleft'] > d and regions['fright'] > d:
-        """
-        전방 영역의 값이 distance보다 작고,
-        좌전방 영역의 값이 distance보다 크고,
-        우전방 영역의 값이 distance보다 큰 경우,
-        차량이 왼쪽으로 이동하도록 change_state() 함수를 통해 상태를 갱신한다.
-        """
-    elif regions['front'] > d and regions['fleft'] > d and regions['fright'] < d:
-        """
-        전방 영역의 값이 distance보다 크고,
-        좌전방 영역의 값이 distance보다 크고,
-        우전방 영역의 값이 distance보다 작은 경우,
-        차량이 직진하도록 change_state() 함수를 통해 상태를 갱신한다.
-        """
-    elif regions['front'] > d and regions['fleft'] < d and regions['fright'] > d:
-        """
-        전방 영역의 값이 distance보다 크고,
-        좌전방 영역의 값이 distance보다 작고,
-        우전방 영역의 값이 distance보다 큰 경우,
-        차량이 오른쪽으로 이동하도록 change_state() 함수를 통해 상태를 갱신한다.
-        """
-    elif regions['front'] < d and regions['fleft'] > d and regions['fright'] < d:
-        """
-        전방 영역의 값이 distance보다 작고,
-        좌전방 영역의 값이 distance보다 크고,
-        우전방 영역의 값이 distance보다 작은 경우,
-        차량이 왼쪽으로 이동하도록 change_state() 함수를 통해 상태를 갱신한다.
-        """
-    elif regions['front'] < d and regions['fleft'] < d and regions['fright'] > d:
-        """
-        전방 영역의 값이 distance보다 작고,
-        좌전방 영역의 값이 distance보다 작고,
-        우전방 영역의 값이 distance보다 큰 경우,
-        차량이 왼쪽으로 이동하도록 change_state() 함수를 통해 상태를 갱신한다.
-        """
-    elif regions['front'] < d and regions['fleft'] < d and regions['fright'] < d:
-        """
-        전방 영역의 값, 좌전방 영역의 값, 우전방 영역의 값이 모두 distance보다 작은 경우,
-        차량이 왼쪽으로 이동하도록 change_state() 함수를 통해 상태를 갱신한다.
-        """
-    elif regions['front'] > d and regions['fleft'] < d and regions['fright'] < d:
-        """
-        전방 영역의 값이 distance보다 크고,
-        좌전방 영역의 값이 distance보다 작고,
-        우전방 영역의 값이 distance보다 작은 경우,
-        차량이 오른쪽으로 이동하도록 change_state() 함수를 통해 상태를 갱신한다.
-        """
-    else:
-        state_description = 'unknown case'
-        rospy.loginfo(regions)
-
-'''
-find_wall(), turn_left(), follow_the_wall()
-함수들을 통해 각 상태별 차량의 행동을 수행한다.
-'''
-
-def find_wall():
-    '''
-    Publisher speed, position을 사용하여 차량이 오른쪽으로 이동하도록 임의의 값을 publish한다.
-    '''
-
-def turn_left():
-    '''
-    Publisher speed, position을 사용하여 차량이 왼쪽으로 이동하도록 임의의 값을 publish한다.
-    '''
-
-def follow_the_wall():
-    '''
-    Publisher speed, position을 사용하여 차량이 전방으로 이동하도록 임의의 값을 publish한다.
-    '''
+    speed = rospy.Publisher('(시뮬레이터의 speed Subscriber와 동일하게 지정)', Float64, queue_size=1)
+    position = rospy.Publisher('(시뮬레이터의 position Subscriber와 동일하게 지정)', Float64, queue_size=1)
+    """
+    LaserScan 메시지 자료형의 data.ranges를 사용하여 회피주행을 진행한다.
+    ranges의 값은 총 360개가 들어오며,
+    0도의 경우 차량 정면,
+    90도의 경우 차량 우측,
+    180도는 차량 뒷편,
+    270도는 차량 좌측에 해당한다. 
+    """
 
 if __name__ == '__main__':
-    rospy.init_node("WallFollwer")
-    sub = rospy.Subscriber("(시뮬레이터 내 LiDAR 센서의 Topic)", LaserScan, callback)
-    rospy.spin()
+    try:
+        rospy.init_node("ObjectAvoid")
+        sub = rospy.Subscriber("(시뮬레이터 내 LiDAR 센서의 Topic)", LaserScan, callback)
+        rospy.spin()
+    except:
+        print("Error occured.")
